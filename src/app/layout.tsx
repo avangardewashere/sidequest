c4927294d38c8faa8265f01b485410c32da0a817
avangardewashere <avelpanaligan@gmail.com>
2026-04-24 16:00:04 +0800

    fix hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,11 +27,12 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <AuthSessionProvider>{children}</AuthSessionProvider>
-        <Locator editor="cursor" />
+        {process.env.NODE_ENV === "development" ? <Locator editor="cursor" /> : null}
       </body>
     </html>
   );
